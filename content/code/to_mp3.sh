ab4ad852418790faf88e8818f4abc2607bdb54f0
dt-woods <davisler@gmail.com>
2025-06-08 12:41:01 -0400

    change default pcm filename

--- content/code/to_mp3.sh
+++ content/code/to_mp3.sh
@@ -1,3 +1,3 @@
 # to_mp3.sh
 # Convert the pcm binary audio file to MP3 using ffmpeg.
-ffmpeg -f f32le -ar 44100 -ac 1 -i sine_wave.pcm output.mp3
+ffmpeg -f f32le -ar 44100 -ac 1 -i output_audio.pcm output.mp3
